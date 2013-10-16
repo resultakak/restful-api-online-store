f307ec7ad66b3fea2950aa5201bdccbec3fe1feb
Kanishk Dudeja <kanishk.dudeja@gmail.com>
2013-10-16 16:38:21 +0530

    http basic authentication

--- httpbasicauth.php
+++ httpbasicauth.php
@@ -6,8 +6,14 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
     echo 'Text to send if user hits Cancel button';
     exit;
 } else {
-    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+	if($_SERVER['PHP_AUTH_USER']=='Kanishk' && $_SERVER['PHP_AUTH_PW']=='Dudeja')
+	{
+	echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
     echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+	}
+	else {
+	header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+	}   
 }
-
 ?>
