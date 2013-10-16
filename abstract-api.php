1e8e5f7f26426a5cc02519a29b7ecf936eba208c
Kanishk Dudeja <kanishk.dudeja@gmail.com>
2013-10-16 18:19:34 +0530

    abstract class

--- abstract-api.php
+++ abstract-api.php
@@ -2,45 +2,31 @@
 abstract class API
 {
     /**
-     * Property: method
-     * The HTTP method this request was made in, either GET, POST, PUT or DELETE
+     * The HTTP method of the request, either GET, POST, PUT or DELETE
      */
     protected $method = '';
     /**
-     * Property: endpoint
-     * The Model requested in the URI. eg: /files
+     * The Resource requested in the URI. eg: /products/2/categories/1
      */
-    protected $endpoint = '';
+    protected $resource = '';
     /**
-     * Property: verb
-     * An optional additional descriptor about the endpoint, used for things that can
-     * not be handled by the basic methods. eg: /files/process
+     * Any query parameters appended with the URL for a PUT Request
      */
-    protected $verb = '';
+    protected $query_params = Array();
     /**
-     * Property: args
-     * Any additional URI components after the endpoint and verb have been removed, in our
-     * case, an integer ID for the resource. eg: /<endpoint>/<verb>/<arg0>/<arg1>
-     * or /<endpoint>/<arg0>
-     */
-    protected $args = Array();
-    /**
-     * Property: file
      * Stores the input of the PUT request
      */
-     protected $file = Null;
-
-    /**
+    protected $input_file = Null;
+	/**
      * Constructor: __construct
-     * Allow for CORS, assemble and pre-process the data
      */
     public function __construct($request) {
-        header("Access-Control-Allow-Orgin: *");
-        header("Access-Control-Allow-Methods: *");
         header("Content-Type: application/json");
-
+		
+		
         $this->args = explode('/', rtrim($request, '/'));
-        $this->endpoint = array_shift($this->args);
+        $this->resource = array_shift($this->args);
+		
         if (array_key_exists(0, $this->args) && !is_numeric($this->args[0])) {
             $this->verb = array_shift($this->args);
         }
@@ -76,8 +62,8 @@ abstract class API
    }
 
 	public function processAPI() {
-        if ((int)method_exists($this->endpoint) > 0) {
-            return $this->_response($this->{$this->endpoint}($this->args));
+        if ((int)method_exists($this->resource) > 0) {
+            return $this->_response($this->{$this->resource}($this->args));
         }
         return $this->_response('', 400);
     }
@@ -90,8 +76,8 @@ abstract class API
     private function _cleanInputs($data) {
         $clean_input = Array();
         if (is_array($data)) {
-            foreach ($data as $k => $v) {
-                $clean_input[$k] = $this->_cleanInputs($v);
+            foreach ($data as $key => $value) {
+                $clean_input[$key] = $this->_cleanInputs($value);
             }
         } else {
             $clean_input = trim(strip_tags($data));
@@ -101,26 +87,12 @@ abstract class API
 
     private function _requestStatus($code) {
         $status = array( 
-            100 => 'Continue',   
-            101 => 'Switching Protocols',   
             200 => 'OK', 
             201 => 'Created',   
             202 => 'Accepted',   
-            203 => 'Non-Authoritative Information',   
             204 => 'No Content',   
-            205 => 'Reset Content',   
-            206 => 'Partial Content',   
-            300 => 'Multiple Choices',   
-            301 => 'Moved Permanently',   
-            302 => 'Found',   
-            303 => 'See Other',   
-            304 => 'Not Modified',   
-            305 => 'Use Proxy',   
-            306 => '(Unused)',   
-            307 => 'Temporary Redirect',   
             400 => 'Bad Request',   
             401 => 'Unauthorized',   
-            402 => 'Payment Required',   
             403 => 'Forbidden',   
             404 => 'Not Found',   
             405 => 'Method Not Allowed',   
@@ -134,14 +106,8 @@ abstract class API
             413 => 'Request Entity Too Large',   
             414 => 'Request-URI Too Long',   
             415 => 'Unsupported Media Type',   
-            416 => 'Requested Range Not Satisfiable',   
-            417 => 'Expectation Failed',   
-            500 => 'Internal Server Error',   
-            501 => 'Not Implemented',   
-            502 => 'Bad Gateway',   
-            503 => 'Service Unavailable',   
-            504 => 'Gateway Timeout',   
-            505 => 'HTTP Version Not Supported'); 
+            500 => 'Internal Server Error'   
+            ); 
         return ($status[$code])?$status[$code]:$status[500];
 }
 }
